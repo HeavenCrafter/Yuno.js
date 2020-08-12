6ea59a67b99e9f3869382fbdf171baa87fdbd887
HeavenCrafter <thunderheavencrafter@gmail.com>
2020-08-12 17:13:00 +0800

    Merged Rushnett's Starboard v1.2.0
    Rushnett's new update fixes threshold related errors

--- src/index.js
+++ src/index.js
@@ -22,76 +22,47 @@ function login () {
   client.login(process.env.BOT_TOKEN)
 }
 
+async function * messagesIterator (channel, messagesLeft) {
+  let before = null
+  let done = false
+  while (messagesLeft > 0) {
+    process.stdout.write(".")
+    const messages = await channel.messages.fetch({ limit: 100, before })
+    if (messages.size > 0) {
+      before = messages.lastKey()
+      messagesLeft = messagesLeft - 100
+      yield messages
+    } else break
+  }
+}
+
+async function * loadMessages (channel, amount) {
+  for await (const messages of messagesIterator(channel, amount)) {
+    for (const message of messages.values()) yield message
+  }
+}
+
 // load old messages into memory
 async function loadIntoMemory () {
-  const channel = client.guilds.cache.get(guildID).channels.cache.get(starboardID)
-  let limit = process.env.fetchLimit
-  console.log(`Loading ${limit} messages...`)
+  const channel = client.guilds.cache.get(guildID).channels.cache.get(smugboardID)
+  let amount = settings.fetchLimit
+  console.log(`Fetching the last ${amount} messages...`)
 
-  let messagesLeft = 0
-  if (limit > 100) {
-    messagesLeft = limit - 100
-    limit = 100
-  }
-
-  console.log(`${messagesLeft} messages left to load`)
-  var messages = await channel.messages.fetch({ limit: limit })
-
-  while (messagesLeft) {
-    if (messagesLeft > 100) {
-      messagesLeft = messagesLeft - 100
-      console.log(`${messagesLeft} messages left to load`)
-      const moreMessages = await channel.messages.fetch({ limit: 100, before: messages.lastKey() })
-      messages = await messages.concat(moreMessages)
-    } else {
-      console.log(`${messagesLeft} messages left to load`)
-      const moreMessages = await channel.messages.fetch({ limit: messagesLeft, before: messages.lastKey() })
-      messages = await messages.concat(moreMessages)
-      messagesLeft = 0
+  // iterate through all messages as they're pulled
+  for await (const message of loadMessages(channel, amount)) {
+    if (message.embeds.length > 0) {
+      if (message.embeds[0].footer) {
+        messagePosted[String(message.embeds[0].footer.text).match(/\((\d{18})\)/)[1]] = {
+          p: true,
+          lc: settings.threshold + 1,
+          legacy: false,
+          psm: message.id
+        }
+      }
     }
   }
-
-  const posts = messages.filter(m => m.content.match(/\((\d{18})\)/))
-  const newPosts = messages.filter(m => {
-    if (m.embeds.length > 0) {
-      if (m.embeds[0].footer) { return String(m.embeds[0].footer.text).match(/\((\d{18})\)/) } else { return false }
-    } else {
-      return false
-    }
-  })
-
-  const postsMap = posts.reduce((mapAccumulator, obj) => {
-    // either one of the following syntax works
-    // mapAccumulator[obj.key] = obj.val;
-    mapAccumulator[String(obj.content.match(/\((\d{18})\)/)[1])] = {
-      p: true,
-      lc: process.env.threshold + 1,
-      legacy: true,
-      psm: obj.id
-    }
-
-    return mapAccumulator
-  }, {})
-
-  const newPostsMap = newPosts.reduce((mapAccumulator, obj) => {
-    // either one of the following syntax works
-    // mapAccumulator[obj.key] = obj.val;
-    mapAccumulator[String(obj.embeds[0].footer.text).match(/\((\d{18})\)/)[1]] = {
-      p: true,
-      lc: process.env.threshold + 1,
-      legacy: false,
-      psm: obj.id
-    }
-
-    return mapAccumulator
-  }, {})
-
-  messagePosted = { ...postsMap, ...newPostsMap }
-
-  console.log(`Loaded ${Object.keys(postsMap).length} legacy posts, and ${Object.keys(newPostsMap).length} new posts in ${process.env.reactionEmoji} channel`)
-
-  console.log('Loading complete')
   loading = false
+  console.log(`\nLoaded ${Object.keys(messagePosted).length} previous posts in ${settings.reactionEmoji} channel!`)
 }
 
 // manage the message board on reaction add/remove
@@ -100,26 +71,24 @@ function manageBoard (reaction_orig) {
   const msg = reaction_orig.message
   const msgChannel = client.guilds.cache.get(guildID).channels.cache.get(msg.channel.id)
   const msgLink = `https://discordapp.com/channels/${guildID}/${msg.channel.id}/${msg.id}`
-  const postChannel = client.guilds.cache.get(guildID).channels.cache.get(starboardID)
+  const postChannel = client.guilds.cache.get(guildID).channels.cache.get(smugboardID)
 
   msgChannel.messages.fetch(msg.id).then((msg) => {
     // if message is older than set amount
     const dateDiff = (new Date()) - reaction_orig.message.createdAt
     const dateCutoff = 1000 * 60 * 60 * 24
-    if (Math.floor(dateDiff / dateCutoff) >= process.env.dateCutoff) {
-      console.log(`a message older than ${process.env.dateCutoff} days was reacted to, ignoring`)
+    if (Math.floor(dateDiff / dateCutoff) >= settings.dateCutoff) {
+      console.log(`a message older than ${settings.dateCutoff} days was reacted to, ignoring`)
       return
     }
 
-    // We need to do this because the reaction count seems to be 1 if an old cached
+    // we need to do this because the reaction count seems to be 1 if an old cached
     // message is starred. This is to get the 'actual' count
-    let found = false
     msg.reactions.cache.forEach((reaction) => {
-      if (reaction.emoji.name == process.env.reactionEmoji) {
-        found = true
-        console.log(`message ${process.env.reactionEmoji}'d! (${msg.id}) in #${msgChannel.name} total: ${reaction.count}`)
+      if (reaction.emoji.name == settings.reactionEmoji) {
+        console.log(`message ${settings.reactionEmoji}'d! (${msg.id}) in #${msgChannel.name} total: ${reaction.count}`)
         // did message reach threshold
-        if (reaction.count >= process.env.threshold) {
+        if (reaction.count >= settings.threshold) {
           messagePosted[msg.id].lc = reaction.count
           // if message is already posted
           if (messagePosted[msg.id].hasOwnProperty('psm')) {
@@ -157,7 +126,7 @@ function manageBoard (reaction_orig) {
 
             const embed = new Discord.MessageEmbed()
               .setAuthor(msg.author.username, avatarURL)
-              .setColor(process.env.hexcolor)
+              .setColor(settings.hexcolor)
               .setDescription(contentMsg)
               .setImage(eURL)
               .setTimestamp(new Date())
@@ -171,19 +140,14 @@ function manageBoard (reaction_orig) {
         }
       }
     })
-
-    // if reactions reach 0
-    if (!found)
-      deletePost(msg)
-
   })
 }
 
 // delete a post
 function deletePost (msg) {
-  const postChannel = client.guilds.cache.get(guildID).channels.cache.get(starboardID)
+  const postChannel = client.guilds.cache.get(guildID).channels.cache.get(smugboardID)
   // if posted to channel board before
-  if (messagePosted[msg.id]) {
+  if (messagePosted[msg.id].p) {
     const editableMessageID = messagePosted[msg.id].psm
     postChannel.messages.fetch(editableMessageID).then((message) => {
       delete messagePosted[msg.id]
@@ -197,8 +161,8 @@ function deletePost (msg) {
 // ON READY
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`)
-  guildID = (process.env.serverID)
-  starboardID = (process.env.channelID)
+  guildID = settings.serverID
+  smugboardID = settings.channelID
   // fetch existing posts
   loadIntoMemory()
 })
@@ -207,9 +171,9 @@ client.on('ready', () => {
 client.on('messageReactionAdd', (reaction_orig, user) => {
   if (loading) return
   // if channel is posting channel
-  if (reaction_orig.message.channel.id == starboardID) return
+  if (reaction_orig.message.channel.id == smugboardID) return
   // if reaction is not desired emoji
-  if (reaction_orig.emoji.name !== process.env.reactionEmoji) return
+  if (reaction_orig.emoji.name !== settings.reactionEmoji) return
 
   const msg = reaction_orig.message
 
@@ -223,7 +187,7 @@ client.on('messageReactionAdd', (reaction_orig, user) => {
     }
   } else {
     if (messagePosted[msg.id].legacy) {
-      console.log(`Legacy message ${process.env.reactionEmoji}'d, ignoring`)
+      console.log(`Legacy message ${settings.reactionEmoji}'d, ignoring`)
       return
     }
   }
@@ -232,20 +196,25 @@ client.on('messageReactionAdd', (reaction_orig, user) => {
 })
 
 // ON REACTION REMOVE
-client.on('messageReactionRemove', (reaction_orig, user) => {
+client.on('messageReactionRemove', (reaction, user) => {
   if (loading) return
   // if channel is posting channel
-  if (reaction_orig.message.channel.id == starboardID) return
+  if (reaction.message.channel.id == smugboardID) return
   // if reaction is not desired emoji
-  if (reaction_orig.emoji.name !== process.env.reactionEmoji) return
+  if (reaction.emoji.name !== settings.reactionEmoji) return
 
 
-  manageBoard(reaction_orig)
+  // if reactions reach 0
+  if (reaction.count === 0)
+    return deletePost(reaction.message)
+  else
+    manageBoard(reaction)
 })
 
 // ON REACTION PURGE
 client.on('messageReactionRemoveAll', (msg) => {
   deletePost(msg)
 })
+
 
 login()
